1191ce0318c569a0c7ff0b25ab4c4020f9a7ee2b
Ivan Barbashov <ivan.barbashov@gmail.com>
2025-10-07 13:09:33 +0000

    basic setup of another page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello World!</h1>
+      <Link href="/about">Link to About Page</Link>
     </main>
-  )
+  );
 }
